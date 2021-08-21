9f06cadfc54428419b2c9d1f6b96d705d999b0b4
Yeshwanth Mootakoduru <yeshwanthmunna2001@gmail.com>
2021-08-22 00:17:07 +0530

    HTML templating using jinja2, using for, if, else

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 @app.route("/home")
-def home():
-    return "<h1>Hello!</h1>"
+def home(): # defining pages on the website
+    return render_template("home.html", var = ["Hi", "Their", "Yeshwanth"], greet = "Welcome!")
 
 @app.route("/<name>")
 def user(name):
+    if name == "Yeshwanth":
+        return redirect(url_for("home"))
     return "Hi! {}".format(name)
 
 if __name__ == '__main__':
