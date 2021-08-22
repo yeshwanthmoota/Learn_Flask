057dd9ed2f7b1ca2114469ac9d9f121552dcfaaa
Yeshwanth Mootakoduru <yeshwanthmunna2001@gmail.com>
2021-08-22 22:28:01 +0530

    HTTP Methods (GET/POST) & Retrieving Form Data

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
@@ -6,13 +6,19 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def home(): # defining pages on the website
-    return render_template("home.html", var = ["Hi", "Their", "Yeshwanth"], greet = "Welcome!")
+    return render_template("home.html")
 
-@app.route("/<name>")
-def user(name):
-    if name == "Yeshwanth":
-        return redirect(url_for("home"))
-    return "Hi! {}".format(name)
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        usr_name = request.form["nm"]
+        return redirect(url_for("user", usr = usr_name))
+    else:
+        return render_template("login.html")
+
+@app.route("/<usr>")
+def user(usr):
+    return "<h2>Hi! {}, you are logged in!</h2>".format(usr)
 
 if __name__ == '__main__':
     app.run(debug = True) # "debug = True" helps make simultaneous changes in the website without restarting the server.
